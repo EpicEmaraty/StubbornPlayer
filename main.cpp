d5874c8529868bcb0223bf60037dc55eb4aeb3d6
epicemaraty <realnuaimi@mail.csuchico.edu>
2018-02-28 19:32:07 -0800

    Done the Lab

--- main.cpp
+++ main.cpp
@@ -6,6 +6,20 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int times;
+  int wins = 0;
+cout<<"How many times do you want to play the game?"<<endl;    
+  cin>> times;
+  
+  for ( int i = 0 ; i<times ; i++ )
+  {
+  game.newGame();
+
+  game.guessDoorC();
+  game.guessDoorC();
+
+  if ( game.isWinner() == 1) { wins++;}
+  }
+  cout<<"You have won "<<wins<<" times!"<<endl;
   return 0;
 }
